ebfcb7b3336a909810782916f97d5ffdc18f6b42
BastienC___ <bastien.clementi@gmail.com>
2025-12-14 21:39:41 +0100

    feat: add new methods

--- src/domain/strategy/future_product.hpp
+++ src/domain/strategy/future_product.hpp
@@ -4,28 +4,51 @@
 
 #include <string>
 #include <vector>
+#include <chrono>
+
+enum SIDE_BREAKING {
+    HIGH,
+    LOW,
+};
 
 class FutureProduct {
 private:
     const std::string name;
+    // State is store as an array of bits
     unsigned int state{0};
+    // Last computed time
+    // Keep it as a variable to avoir reallocating memory for new variables
+    std::chrono::high_resolution_clock lastComputedTime;
+    
+    // update with GetPrices()
+    SIDE_BREAKING currentSideBreaking;
     std::vector<Price> priceHistory;
 
     // Reference to ports
     MarketDataPort& marketDataPort;
-public:
+    
+    // Calling ports
+    Price GetPrices();
+    void GetGraph(); // TO DO: not void
+    
+    // Strategy from graph
+    void RangeIsBigEnough();
+    void PriceHasRetraced();
+    
+    // Strategy from dom
+    void DynamicPush();
+    void DynamicBack(); // After push
+    void IsDomFull();
+    public:
     // Constructors / Destructors
     FutureProduct(std::string _name, MarketDataPort& port);
     ~FutureProduct();
-
+    
     // Getters / Setters
     bool GetState() const;
     std::string GetName() const;
-
-    // Calling ports
-    Price GetPrices() const;
-
-    // Strategy
-    void RangeIsBigEnough();
-    void PriceHasRetraced();
+    
+    // Run
+    // calls all the methods is the right order
+    void Run();
 };
